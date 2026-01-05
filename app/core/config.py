7c1e524847a8906d20111698f52675fb8483306f
Ritabrata Pal <rito3668@gmail.com>
2026-01-05 10:28:22 +0530

    feat: add redis config and cache ttl

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APP_NAME:str = "Text summarization api"
     DEBUG:bool = True
+    REDIS_URL:str = "redis://localhost:6379/0"
+    CACHE_TTL:int = 300
     class Config:
         env_file = ".env"
 
